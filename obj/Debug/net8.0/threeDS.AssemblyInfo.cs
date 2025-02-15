935a8a7102d68a727c78ad7064c597795ec7ccaa
Kenneth <kenny@kennethchilds.com>
2025-02-15 00:23:04 -0500

    file name + velocity tweak

--- obj/Debug/net8.0/threeDS.AssemblyInfo.cs
+++ obj/Debug/net8.0/threeDS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("threeDS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eeb37a40db67aac9bc0302208fdc73d1e7a58f52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e1acb8c3cef9ef1687e35d642b4fb55fd09e003")]
 [assembly: System.Reflection.AssemblyProductAttribute("threeDS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("threeDS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
